a66ed59db48829451ac13f291ac4d9db614d0408
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-09-26 14:07:25 +0300

    improved buttons UI

--- excuses.js
+++ excuses.js
@@ -15,11 +15,11 @@ categoryButtons.forEach(button => button.addEventListener('click', () => {
     if (button.innerHTML !== 'Get Random Excuse') {
         excuseCategory = button.innerHTML.toLowerCase();
         categoryButtons.forEach((everyButton) => {
-            everyButton.style.background = 'white';
-            everyButton.style.color = 'black';
+            everyButton.style.background = 'none';
+            everyButton.style.color = 'white';
         });
-        button.style.background = '#454555';
-        button.style.color = 'white';
+        button.style.background = 'white';
+        button.style.color = '#1a1a1a';
     }
 })
 );
